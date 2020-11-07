ebae50d95611ee812183fbf755b9c434db63409d
danielescillia <daniele.scillia@gmail.com>
2020-11-07 12:33:31 +0100

    Removed unused arabic and close kata in order to mone on with study of TPP

--- src/Kata/RomanNumerals.php
+++ src/Kata/RomanNumerals.php
@@ -5,14 +5,6 @@ namespace Kata;
 class RomanNumerals
 {
     private $arabicToRomansBreakpoints = [
-        1000 		=> 	'M',
-        900 		=> 	'CM',
-        500		=>	'D',
-        400		=>	'CD',
-        100		=>	'C',
-        90		=>	'XC',
-        50		=>	'L',
-        40		=>	'XL',
         10		=>	'X',
         5		=>	'V',
         1		=>	'I'
