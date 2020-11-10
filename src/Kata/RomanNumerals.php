0b3dd9c52632c55c3fc59d5d75fe62cac7f83460
danielescillia <daniele.scillia@gmail.com>
2020-11-10 10:30:33 +0100

    roman numerals with tpp; currently at step 7: scalar => array

--- src/Kata/RomanNumerals.php
+++ src/Kata/RomanNumerals.php
@@ -4,8 +4,20 @@ namespace Kata;
 
 class RomanNumerals
 {
+    private $converter = [
+        5    => 'V',
+        10   => 'X',
+        50   => 'L',
+        100  => 'C',
+        500  => 'D',
+        1000 => 'M',
+    ];
+
     public function handle(int $arabic): string
     {
+        if ($arabic > 3) {
+            return $this->converter[$arabic];
+        }
         return str_repeat('I', $arabic);
     }
 }
--- tests/Kata/RomanNumeralsTest.php
+++ tests/Kata/RomanNumeralsTest.php
@@ -35,4 +35,46 @@ class RomanNumeralsTest extends TestCase
         $this->assertEquals('III', $actual);
     }
 
+    public function testArabic5ReturnsRomanV(): void
+    {
+        $actual = $this->romanNumerals->handle(5);
+
+        $this->assertEquals('V', $actual);
+    }
+
+    public function testArabic10ReturnsRomanX(): void
+    {
+        $actual = $this->romanNumerals->handle(10);
+
+        $this->assertEquals('X', $actual);
+    }
+
+    public function testArabic50ReturnsRomanL(): void
+    {
+        $actual = $this->romanNumerals->handle(50);
+
+        $this->assertEquals('L', $actual);
+    }
+
+    public function testArabic100ReturnsRomanC(): void
+    {
+        $actual = $this->romanNumerals->handle(100);
+
+        $this->assertEquals('C', $actual);
+    }
+
+    public function testArabic500ReturnsRomanD(): void
+    {
+        $actual = $this->romanNumerals->handle(500);
+
+        $this->assertEquals('D', $actual);
+    }
+
+    public function testArabic1000ReturnsRomanM(): void
+    {
+        $actual = $this->romanNumerals->handle(1000);
+
+        $this->assertEquals('M', $actual);
+    }
+
 }
